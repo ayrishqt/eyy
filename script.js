1d59870c1b452e950b69530d65c62dd4eb3c519e
ayrishqt <108666920+ayrishqt@users.noreply.github.com>
2024-01-16 14:55:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "oki tenks tenked sm!";
+  question.innerHTML = "oki, tenks sm!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
